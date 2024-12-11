e9fb1f893f503fb3415be15c44c8634787463e39
Egor Yaroshko <ego8973@gmail.com>
2024-12-11 03:16:44 +0300

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -8,24 +8,21 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing essential packages..."
 sudo apt install -y build-essential pkg-config libssl-dev git-all tmux protobuf-compiler
 
-# Настройка Cargo
-echo "Setting up Cargo environment..."
-. "$HOME/.cargo/env"
-echo 'source $HOME/.cargo/env' >> ~/.bashrc
-source ~/.bashrc
-
-# Добавление protoc в PATH
-echo "Adding protoc to PATH..."
-export PATH=$PATH:/path/to/protoc/directory
-echo 'export PATH=$PATH:/path/to/protoc/directory' >> ~/.bashrc
-
-# Проверка версии protoc
-echo "Checking protoc version..."
-protoc --version
-
-# Запуск tmux и выполнение установки Nexus CLI
-echo "Starting tmux session for Nexus CLI installation..."
-tmux new-session -d -s nexus_install 'curl https://cli.nexus.xyz/ | sh'
+# Создание tmux-сессии и продолжение работы в ней
+echo "Starting tmux session for script continuation..."
+tmux new-session -d -s my_session "bash -c ' \
+  echo \"Installing Nexus CLI...\"; \
+  curl https://cli.nexus.xyz/ | sh; \
+  echo \"Setting up Cargo environment...\"; \
+  . \"\$HOME/.cargo/env\"; \
+  echo \"source \$HOME/.cargo/env\" >> ~/.bashrc; \
+  source ~/.bashrc; \
+  echo \"Adding protoc to PATH...\"; \
+  export PATH=\$PATH:/path/to/protoc/directory; \
+  echo \"export PATH=\$PATH:/path/to/protoc/directory\" >> ~/.bashrc; \
+  echo \"Checking protoc version...\"; \
+  protoc --version; \
+  echo \"All tasks completed in tmux session.\"'"
 
 # Присоединение к tmux-сессии
-tmux attach -t nexus_install
+tmux attach -t my_session
