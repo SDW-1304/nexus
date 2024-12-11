44748d753ecced2b6ca861db75ea0f86fd87e153
Egor Yaroshko <ego8973@gmail.com>
2024-12-11 03:02:23 +0300

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -8,10 +8,6 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing essential packages..."
 sudo apt install -y build-essential pkg-config libssl-dev git-all tmux protobuf-compiler
 
-# Установка Nexus CLI
-echo "Installing Nexus CLI..."
-curl https://cli.nexus.xyz/ | sh
-
 # Настройка Cargo
 echo "Setting up Cargo environment..."
 . "$HOME/.cargo/env"
@@ -27,7 +23,9 @@ echo 'export PATH=$PATH:/path/to/protoc/directory' >> ~/.bashrc
 echo "Checking protoc version..."
 protoc --version
 
-# Создание и запуск tmux-сессии
-echo "Starting tmux session..."
-tmux new-session -d -s my_session 'bash <(curl -s https://raw.githubusercontent.com/SDW-1304/nexus/main/nexus.sh)'
-tmux attach -t my_session
+# Запуск tmux и выполнение установки Nexus CLI
+echo "Starting tmux session for Nexus CLI installation..."
+tmux new-session -d -s nexus_install 'curl https://cli.nexus.xyz/ | sh'
+
+# Присоединение к tmux-сессии
+tmux attach -t nexus_install
